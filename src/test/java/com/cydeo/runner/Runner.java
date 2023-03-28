3400a7d309876b60f93376c45fcf229b8034f97e
hiltas73 <h_iltas@yahoo.com>
2023-03-28 16:11:13 +0300

    Login Halim addings

--- src/test/java/com/cydeo/runner/Runner.java
+++ src/test/java/com/cydeo/runner/Runner.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
+                "json:target/cucumber.json",
                 "html:target/cucumber/report.html"
         },
 
